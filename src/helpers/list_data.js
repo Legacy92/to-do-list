ffb3f99f1cc7d6335e5f533faaf154fdf0b6005d
Legacy92 <colemancorey22@gmail.com>
2018-05-02 16:23:34 -0700

    completed displaying dummy data

---
+++ src/helpers/list_data.js
@@ -0,0 +1,19 @@
+
+export default [
+    {
+        title: 'Learn React',
+        details: 'Learn reactJS',
+    },
+    {
+        title: 'Walk the dog',
+        details: 'He needs to be walked at 7pm'
+    },
+    {
+        title: 'Boyfriend Duties',
+        details: 'Engage in social stimulation'
+    },
+    {
+        title: 'Prototypes',
+        details: 'Improve coding skills by......coding'
+    }
+];
